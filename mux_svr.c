13c69a888631cda31d0a5c56b12c73677442f981
Quincy Lam <qlam3@my.bcit.ca>
2019-02-22 20:00:44 -0800

    Made server show data, made tcp_clnt multithreaded with mem() mutex fix

--- mux_svr.c
+++ mux_svr.c
@@ -17,7 +17,7 @@
 --	DESIGNERS:		Based on Richard Stevens Example, p165-166
 --				Modified & redesigned: Aman Abdulla: February 16, 2001
 --
---				
+--
 --	PROGRAMMER:		Aman Abdulla
 --
 --	NOTES:
@@ -27,145 +27,175 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
 
-#define SERVER_TCP_PORT 7000	// Default port
-#define BUFLEN	255		//Buffer length
-#define TRUE	1
-#define LISTENQ	5
-#define MAXLINE 4096
+#define SERVER_TCP_PORT 7001 // Default port
+# define BUFLEN 255 //Buffer length
+# define TRUE 1
+# define LISTENQ 5
+# define MAXLINE 4096
 
 // Function Prototypes
-static void SystemFatal(const char* );
+static void SystemFatal(const char * );
 
-int main (int argc, char **argv)
-{
-	int i, maxi, nready, bytes_to_read, arg;
-	int listen_sd, new_sd, sockfd, client_len, port, maxfd, client[FD_SETSIZE];
-	struct sockaddr_in server, client_addr;
-	char *bp, buf[BUFLEN];
-   	ssize_t n;
-   	fd_set rset, allset;
+int main(int argc, char ** argv) {
+    int i, maxi, nready, bytes_to_read, arg;
+    int listen_sd, new_sd, sockfd, client_len, port, maxfd, client[FD_SETSIZE];
+    int portNum[FD_SETSIZE]; // Saves port numbers of the connected clients
+    double startTimer[FD_SETSIZE]; // Saves start Timer of each of the clients
+    int requestedGenerated[FD_SETSIZE];
+    size_t dataTransfered[FD_SETSIZE];
+    int clientNumber[FD_SETSIZE];
+    int numOfClients = 0;
+    clock_t end;
+    struct sockaddr_in server, client_addr;
+    char * bp, buf[BUFLEN];
+    ssize_t n;
+    fd_set rset, allset;
+    char tmpc;
+    double cpu_time_used;
 
-	switch(argc)
+    switch (argc)
 	{
-		case 1:
-			port = SERVER_TCP_PORT;	// Use the default port
-		break;
-		case 2:
-			port = atoi(argv[1]);	// Get user specified port
-		break;
-		default:
-			fprintf(stderr, "Usage: %s [port]\n", argv[0]);
-			exit(1);
-	}
+	    case 1:
+	        port = SERVER_TCP_PORT; // Use the default port
+	        break;
+	    case 2:
+	        port = atoi(argv[1]); // Get user specified port
+	        break;
+	    default:
+	        fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+	        exit(1);
+    }
 
-	// Create a stream socket
-	if ((listen_sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-		SystemFatal("Cannot Create Socket!");
-	
-	// set SO_REUSEADDR so port can be resused imemediately after exit, i.e., after CTRL-c
-        arg = 1;
-        if (setsockopt (listen_sd, SOL_SOCKET, SO_REUSEADDR, &arg, sizeof(arg)) == -1)
-                SystemFatal("setsockopt");
+    // Create a stream socket
+    if ((listen_sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        SystemFatal("Cannot Create Socket!");
 
-	// Bind an address to the socket
-	bzero((char *)&server, sizeof(struct sockaddr_in));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
+    // set SO_REUSEADDR so port can be resused imemediately after exit, i.e., after CTRL-c
+    arg = 1;
+    if (setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, & arg, sizeof(arg)) == -1)
+        SystemFatal("setsockopt");
 
-	if (bind(listen_sd, (struct sockaddr *)&server, sizeof(server)) == -1)
-		SystemFatal("bind error");
-	
-	// Listen for connections
-	// queue up to LISTENQ connect requests
-	listen(listen_sd, LISTENQ);
+    // Bind an address to the socket
+    bzero((char * ) & server, sizeof(struct sockaddr_in));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY); // Accept connections from any client
 
-	maxfd	= listen_sd;	// initialize
-   	maxi	= -1;		// index into client[] array
+    if (bind(listen_sd, (struct sockaddr * ) & server, sizeof(server)) == -1)
+        SystemFatal("bind error");
 
-	for (i = 0; i < FD_SETSIZE; i++)
-           	client[i] = -1;             // -1 indicates available entry
- 	FD_ZERO(&allset);
-   	FD_SET(listen_sd, &allset);
+    // Listen for connections
+    // queue up to LISTENQ connect requests
+    listen(listen_sd, LISTENQ);
 
+    maxfd = listen_sd; // initialize
+    maxi = -1; // index into client[] array
 
-	while (TRUE)
+    for (i = 0; i < FD_SETSIZE; i++)
+        client[i] = -1; // -1 indicates available entry
+    FD_ZERO( & allset);
+    FD_SET(listen_sd, & allset);
+
+    while (TRUE)
 	{
-   		rset = allset;               // structure assignment
-		nready = select(maxfd + 1, &rset, NULL, NULL, NULL);
+        rset = allset; // structure assignment
+        nready = select(maxfd + 1, & rset, NULL, NULL, NULL);
 
-      		if (FD_ISSET(listen_sd, &rset)) // new client connection
-      		{
-			client_len = sizeof(client_addr);
-			if ((new_sd = accept(listen_sd, (struct sockaddr *) &client_addr, &client_len)) == -1)
-				SystemFatal("accept error");
-			
-                        printf(" Remote Address:  %s\n", inet_ntoa(client_addr.sin_addr));
+        if (FD_ISSET(listen_sd, & rset)) // new client connection
+        {
+            client_len = sizeof(client_addr);
+            if ((new_sd = accept(listen_sd, (struct sockaddr * ) & client_addr, & client_len)) == -1)
+                SystemFatal("accept error");
 
-                        for (i = 0; i < FD_SETSIZE; i++)
-			if (client[i] < 0)
-            		{
-				client[i] = new_sd;	// save descriptor
-				break;
-            		}
-			if (i == FD_SETSIZE)
-         		{
-				printf ("Too many clients\n");
-            			exit(1);
-    			}
+            // printf(" Remote Address:  %s:%hu\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-			FD_SET (new_sd, &allset);     // add new descriptor to set
-			if (new_sd > maxfd)
-				maxfd = new_sd;	// for select
+            //Accepted the a new client, increment to tell what client number they are.
+            numOfClients += 1;
 
-			if (i > maxi)
-				maxi = i;	// new max index in client[] array
-
-			if (--nready <= 0)
-				continue;	// no more readable descriptors
-     		 }
-
-		for (i = 0; i <= maxi; i++)	// check all clients for data
-     		{
-			if ((sockfd = client[i]) < 0)
-				continue;
-
-			if (FD_ISSET(sockfd, &rset))
-         		{
-         			bp = buf;
-				bytes_to_read = BUFLEN;
-				while ((n = read(sockfd, bp, bytes_to_read)) > 0)
+            for (i = 0; i < FD_SETSIZE; i++)
+                if (client[i] < 0)
 				{
+                    client[i] = new_sd; // save descriptor
+                    portNum[i] = ntohs(client_addr.sin_port); // saves the client's port number
+                    startTimer[i] = clock();
+                    clientNumber[i] = numOfClients;
+                    break;
+                }
+            if (i == FD_SETSIZE)
+			{
+                printf("Too many clients\n");
+                exit(1);
+            }
+
+            FD_SET(new_sd, & allset); // add new descriptor to set
+            if (new_sd > maxfd)
+                maxfd = new_sd; // for select
+
+            if (i > maxi)
+                maxi = i; // new max index in client[] array
+
+            if (--nready <= 0)
+                continue; // no more readable descriptors
+        }
+
+        for (i = 0; i <= maxi; i++) // check all clients for data
+        {
+            if ((sockfd = client[i]) < 0)
+                continue;
+
+            if (FD_ISSET(sockfd, & rset)) {
+                bp = buf;
+                bytes_to_read = BUFLEN;
+                while ((n = read(sockfd, bp, bytes_to_read)) > 0)
+                {
 					bp += n;
-					bytes_to_read -= n;
-				}
-				write(sockfd, buf, BUFLEN);   // echo to client
-				
-				if (n == 0) // connection closed by client
-            			{
-					printf(" Remote Address:  %s closed connection\n", inet_ntoa(client_addr.sin_addr));
-					close(sockfd);
-					FD_CLR(sockfd, &allset);
-               				client[i] = -1;
-            			}
-									            				
-				if (--nready <= 0)
-            				break;        // no more readable descriptors
-			}
-     		 }
-   	}
-	return(0);
+                    bytes_to_read -= n;
+                }
+
+                //Connection should be closed when reaching EOF instead of when text is done being sent
+                if (buf[0] != '\0')
+                {
+                    // printf("%s\n", buf);
+                    requestedGenerated[i] += 1;
+                    dataTransfered[i] += sizeof(buf);
+                    write(sockfd, buf, BUFLEN); // echo to client
+                }
+                else
+                {
+                    end = clock();
+                    cpu_time_used = ((double) (end - startTimer[i])) / CLOCKS_PER_SEC;
+                    // printf("Connection #, Remote Address:Port Number, Time used, Requests Generated, Data Transfered\n");
+                    // printf("====================================================\n");
+                    printf("%d, %s:%hu, %lf, %d, %d\n", clientNumber[i], inet_ntoa(client_addr.sin_addr), portNum[i], cpu_time_used, requestedGenerated[i], dataTransfered[i]);
+                    close(sockfd);
+                    FD_CLR(sockfd, &allset);
+                    client[i] = -1;
+                    portNum[i] = 0;
+                    startTimer[i] = 0;
+                    requestedGenerated[i] = 0;
+                    dataTransfered[i] = 0;
+                    clientNumber[i] = -1;
+                }
+
+                if (--nready <= 0)
+                    break; // no more readable descriptors
+            }
+        }
+    }
+    return (0);
 }
 
 // Prints the error stored in errno and aborts the program.
-static void SystemFatal(const char* message)
+static void SystemFatal(const char * message)
 {
-    perror (message);
-    exit (EXIT_FAILURE);
+    perror(message);
+    exit(EXIT_FAILURE);
 }
--- tcp_clnt.c
+++ tcp_clnt.c
@@ -10,7 +10,7 @@
 --	REVISIONS:		(Date and Description)
 --				January 2005
 --				Modified the read loop to use fgets.
---				While loop is based on the buffer length 
+--				While loop is based on the buffer length
 --
 --
 --	DESIGNERS:		Aman Abdulla
@@ -24,6 +24,7 @@
 -- 	prompted for date. The date string is then sent to the server and the
 -- 	response (echo) back from the server is displayed.
 ---------------------------------------------------------------------------------------*/
+#include <pthread.h>
 #include <stdio.h>
 #include <netdb.h>
 #include <sys/types.h>
@@ -35,79 +36,166 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define SERVER_TCP_PORT		7000	// Default port
-#define BUFLEN			80  	// Buffer length
+// Function Prototypes
+void *ClntConnection(void *data);
 
-int main (int argc, char **argv)
+#define SERVER_TCP_PORT 7000 // Default port
+#define BUFLEN 255           // Buffer length
+
+//Struct
+struct ConArgs
 {
-	int n, bytes_to_read;
-	int sd, port;
-	struct hostent	*hp;
-	struct sockaddr_in server;
-	char  *host, *bp, rbuf[BUFLEN], sbuf[BUFLEN], **pptr, *sptr;
-	char str[16];
+    int port;
+    char *host;
+};
 
-	switch(argc)
-	{
-		case 2:
-			host =	argv[1];	// Host name
-			port =	SERVER_TCP_PORT;
-		break;
-		case 3:
-			host =	argv[1];
-			port =	atoi(argv[2]);	// User specified port
-		break;
-		default:
-			fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
-			exit(1);
-	}
+pthread_mutex_t lock;
 
-	// Create the socket
-	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		perror("Cannot create socket");
-		exit(1);
-	}
-	bzero((char *)&server, sizeof(struct sockaddr_in));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	if ((hp = gethostbyname(host)) == NULL)
-	{
-		fprintf(stderr, "Unknown server address\n");
-		exit(1);
-	}
-	bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+int main(int argc, char **argv)
+{
+    int i, n, bytes_to_read;
+    int sd, port;
+    struct hostent *hp;
+    struct sockaddr_in server;
+    struct ConArgs connectionArgs;
+    struct ConArgs *argPT;
 
-	// Connecting to the server
-	if (connect (sd, (struct sockaddr *)&server, sizeof(server)) == -1)
-	{
-		fprintf(stderr, "Can't connect to server\n");
-		perror("connect");
-		exit(1);
-	}
-	printf("Connected:    Server Name: %s\n", hp->h_name);
-	pptr = hp->h_addr_list;
-	printf("\t\tIP Address: %s\n", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)));
-	printf("Transmit:\n");
-	//gets(sbuf); // get user's text
-	fgets (sbuf, BUFLEN, stdin);
+    char *host, *bp, rbuf[BUFLEN], sbuf[BUFLEN], **pptr, *sptr;
+    char str[16];
+    int numOfThreads = 1;
 
-	// Transmit data through the socket
-	send (sd, sbuf, BUFLEN, 0);
+    switch (argc)
+    {
+    case 2:
+        host = argv[1]; // Host name
+        port = SERVER_TCP_PORT;
+        break;
+    case 3:
+        host = argv[1];
+        port = atoi(argv[2]); // User specified port
+        break;
+    case 4:
+        host = argv[1];
+        port = atoi(argv[2]);
+        numOfThreads = atoi(argv[3]);
+        break;
+    default:
+        fprintf(stderr, "Usage: %s host [port] [number of threads]\n", argv[0]);
+        exit(1);
+    }
+    connectionArgs.host = host;
+    connectionArgs.port = port;
+    argPT = &connectionArgs;
 
-	printf("Receive:\n");
-	bp = rbuf;
-	bytes_to_read = BUFLEN;
+    //Creates list of threads
+    pthread_t threadList[numOfThreads];
 
-	// client makes repeated calls to recv until no more data is expected to arrive.
-	n = 0;
-	while ((n = recv (sd, bp, bytes_to_read, 0)) < BUFLEN)
-	{
-		bp += n;
-		bytes_to_read -= n;
-	}
-	printf ("%s\n", rbuf);
-	fflush(stdout);
-	close (sd);
-	return (0);
+    //Create # of clients
+    for (i = 0; i < numOfThreads; i++)
+    {
+        pthread_create(&threadList[i], NULL, ClntConnection, (void *)argPT);
+    }
+
+    //Let main function finish when all threads finish
+    for (i = 0; i < numOfThreads; i++) 
+    {
+        pthread_join(threadList[i], NULL);
+    }
+    printf("Done\n");
+    return (0);
+}
+
+void *ClntConnection(void *data)
+{
+    struct ConArgs *connectionArgs = data;
+    int port = connectionArgs->port;
+    char *host = connectionArgs->host;
+    int n, bytes_to_read, sd;
+    struct hostent *hp;
+    struct sockaddr_in server;
+    char *bp, rbuf[BUFLEN], sbuf[BUFLEN], **pptr, *sptr;
+    char str[16];
+
+    //Mutex lock important Memory functions to prevent segmentation faults
+    pthread_mutex_lock(&lock);
+
+    // Open the text file and read it for data for sending
+
+    //Ensure ulimit is a high value when testing: ulimit -n ####
+    FILE *fp = fopen("alice.txt", "r");
+
+    // Create the socket
+    if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Cannot create socket");
+        exit(1);
+    }
+
+    bzero((char *)&server, sizeof(struct sockaddr_in));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+
+    if ((hp = gethostbyname(host)) == NULL)
+    {
+        fprintf(stderr, "Unknown server address\n");
+        exit(1);
+    }
+
+    bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+
+    // Connecting to the server
+    if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1)
+    {
+        fprintf(stderr, "Can't connect to server\n");
+        perror("connect");
+        exit(1);
+    }
+    // printf("Connected: Server Name: %s\n", hp->h_name);
+    pptr = hp->h_addr_list;
+    // printf("\t\tIP Address: %s\n", inet_ntop(hp->h_addrtype, *pptr, str, sizeof(str)));
+
+    //Enters Loop to periodically Send text
+    //gets(sbuf); // get user's text
+
+    //Finished Memory stuff, let threads send stuff now
+    pthread_mutex_unlock(&lock);
+
+    while (fgets(sbuf, BUFLEN, fp) != 0)
+    {
+
+        // printf("Now sleeping\n");
+        // sleep(1);
+
+        //Get from file
+        // printf("Transmit:\n");
+        // printf("%s", sbuf);
+        write(sd, sbuf, BUFLEN);
+
+        //Set up receive
+        // printf("Receive:\n");
+        bp = rbuf;
+        bytes_to_read = BUFLEN;
+
+        // client makes repeated calls to recv until no more data is expected to arrive.
+        n = 0;
+        while ((n = recv(sd, bp, bytes_to_read, 0)) < BUFLEN)
+        {
+            bp += n;
+            bytes_to_read -= n;
+        }
+        // printf("%s\n", rbuf);
+        // fflush(stdout);
+    }
+
+    sbuf[0] = '\0';
+
+    // printf("Finished with reading file, sending EOF\n");
+    //Send last string with ending line
+    write(sd, sbuf, BUFLEN);
+
+    //Properly close file when finished
+    fclose(fp);
+    close(sd);
+    return NULL;
 }
